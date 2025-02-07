2351cf07544581bf375eb703f0dc547b69c6b972
Hamza Kamran <hamzakamrn8@gmail.com>
2025-02-07 17:36:15 +0500

    all pages complete responsive

--- Food-Project/src/context/StoreContext.jsx
+++ Food-Project/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import PropTypes from "prop-types"; 
 import { food_list } from "../assets/assets";
 
@@ -20,9 +20,17 @@ const StoreContextProvider = ({ children }) => {
       [itemId]: prev[itemId] - 1,
     }));
   };
-  useEffect(()=>{
-    console.log(cartItems)
-  }, [cartItems])
+  
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for(const item in cartItems){
+      if(cartItems[item]>0){
+        let itemInfo = food_list.find((product)=> product._id === item);
+        totalAmount += itemInfo.price* cartItems[item];
+      }
+    }
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
@@ -30,6 +38,7 @@ const StoreContextProvider = ({ children }) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
