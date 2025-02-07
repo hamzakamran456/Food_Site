74a53f0d1749959603382659b2e9c1321fd58373
Hamza Kamran <hamzakamrn8@gmail.com>
2025-02-07 15:29:07 +0500

    loginpopup setting

--- Food-Project/src/components/Navbar/Navbar.jsx
+++ Food-Project/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,44 @@
-import './Navbar.css'
-import { assets } from '../../assets/assets'
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-const Navbar = () => {
-  const [menu, setMenu] = useState("home")
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
-      <img src={assets.logo} alt="" className="logo"  />
+      <img src={assets.logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <Link to="/" onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>home</Link>
-        <a href="#explore-menu" onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
-        <a href="#app-download" onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
-        <a href="#footer" onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -19,10 +46,13 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign In</button>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+Navbar.propTypes = {
+  setShowLogin: PropTypes.func.isRequired,
+};
+export default Navbar;
